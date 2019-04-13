9c97b2479371e05f14a477eef84c395c0696dcef
FukeKazki <40536586+FukeKazki@users.noreply.github.com>
2019-04-13 13:39:05 +0900

    Add css

---
+++ js/index.js
@@ -0,0 +1,10 @@
+'use strict';
+{
+    $(document).ready(() => {
+       $('.slick-slider').slick({
+           autoplay: true,
+           autoplaySpeed: 2000,
+           arrows: false,
+       });
+    });
+}
